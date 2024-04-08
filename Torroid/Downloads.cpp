9ecaf8161fe1ff854888f20be147b36fed82126d
Himanshu Raj <himanshurajkush@gmail.com>
2024-04-09 00:06:15 +0530

    Torroid: Downloads: Update the Downlods runtime class to reflect Actual UI elements

--- Torroid/Downloads.cpp
+++ Torroid/Downloads.cpp
@@ -8,14 +8,11 @@ using namespace Microsoft::UI::Xaml::Data;
 
 namespace winrt::Torroid::implementation
 {
-    Downloads::Downloads(hstring const& fileName) : m_fileName{ fileName }
+    Downloads::Downloads(hstring const& fileName, hstring const& totalSize) : m_fileName{ fileName }, m_totalSize { totalSize }
     {
     }
 
-    hstring Downloads::FileName()
-    {
-        return m_fileName;
-    }
+    hstring Downloads::FileName(){ return m_fileName; }
     void Downloads::FileName(hstring const& value)
     {
         if (m_fileName != value)
@@ -24,6 +21,57 @@ namespace winrt::Torroid::implementation
             m_propertyChanged(*this, PropertyChangedEventArgs{ L"FileName" });
         }
     }
+
+    hstring Downloads::Percentage() { return m_percentage; }
+    void Downloads::Percentage(hstring const& value)
+    {
+        if (m_percentage != value)
+        {
+            m_percentage = value;
+            m_propertyChanged(*this, PropertyChangedEventArgs{ L"Percentage" });
+        }
+    }
+
+    double Downloads::DPercentage(){ return m_dPercentage; }
+    void Downloads::DPercentage(double const& value)
+    {
+        if (m_dPercentage != value)
+        {
+            m_dPercentage = value;
+            m_propertyChanged(*this, PropertyChangedEventArgs{ L"DPercentage" });
+        }
+    }
+
+    hstring Downloads::CurSize(){ return m_curSize; }
+    void Downloads::CurSize(hstring const& value)
+    {
+        if (m_curSize != value)
+        {
+            m_curSize = value;
+            m_propertyChanged(*this, PropertyChangedEventArgs{ L"CurSize" });
+        }
+    }
+
+    hstring Downloads::TotalSize(){ return m_totalSize; }
+    void Downloads::TotalSize(hstring const& value)
+    {
+        if (m_totalSize != value)
+        {
+            m_totalSize = value;
+            m_propertyChanged(*this, PropertyChangedEventArgs{ L"TotalSize" });
+        }
+    }
+
+    hstring Downloads::Speed(){ return m_speed; }
+    void Downloads::Speed(hstring const& value)
+    {
+        if (m_speed != value)
+        {
+            m_speed = value;
+            m_propertyChanged(*this, PropertyChangedEventArgs{ L"Speed" });
+        }
+    }
+
     winrt::event_token Downloads::PropertyChanged(PropertyChangedEventHandler const& handler)
     {
         return m_propertyChanged.add(handler);
--- Torroid/Downloads.h
+++ Torroid/Downloads.h
@@ -1,21 +1,41 @@
 #pragma once
 #include "Downloads.g.h"
+#include "DownloadFile.h"
 
 namespace winrt::Torroid::implementation
 {
     struct Downloads : DownloadsT<Downloads>
     {
         Downloads() = delete;
+        Downloads(hstring const& fileName, hstring const& totalSize);
 
-        Downloads(hstring const& fileName);
         hstring FileName();
         void FileName(hstring const& value);
+
+        hstring Percentage();
+        void Percentage(hstring const& value);
+        
+        double DPercentage();
+        void DPercentage(double const& value);
+        
+        hstring CurSize();
+        void CurSize(hstring const& value);
+        hstring TotalSize();
+        void TotalSize(hstring const& value);
+        hstring Speed();
+        void Speed(hstring const& value);
+
         winrt::event_token PropertyChanged(winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& handler);
         void PropertyChanged(winrt::event_token const& token) noexcept;
 
     private:
-        winrt::hstring m_fileName;
-        winrt::event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> m_propertyChanged;
+        hstring m_fileName;
+        hstring m_percentage = L"0%";
+        double m_dPercentage = 10;
+        hstring m_curSize = L"0Mb";
+        hstring m_totalSize = L"0Mb";
+        hstring m_speed = L"0Mbps";
+        event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> m_propertyChanged;
     };
 }
 namespace winrt::Torroid::factory_implementation
