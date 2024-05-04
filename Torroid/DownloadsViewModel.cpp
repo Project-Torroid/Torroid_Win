eca207c0e367417b30032cd23880ca4bf7322046
Himanshu Raj <himanshurajkush@gmail.com>
2024-05-04 17:18:43 +0530

    Torroid: DownloadsViewModel: Add the alredy existing entry from JSON to m_downloadsOBVector
    - Start downloads stats update by calling "startDownloadStatsUpdates()" in the constructor of DownloadsViewModel
    
    - Fix updateDownloadStats to show the stats for all possible conditions
    
    - Fix the presion for file size higher than 2GB

--- Torroid/DownloadsViewModel.cpp
+++ Torroid/DownloadsViewModel.cpp
@@ -13,6 +13,16 @@ namespace winrt::Torroid::implementation
     DownloadsViewModel::DownloadsViewModel()
     {
         m_downloadsOBVector = single_threaded_observable_vector<Torroid::Downloads>();
+
+        for (size_t index = 0; index < DownloadsJson::jsonInstance().size(); index++)
+        {
+            std::string f_name = Utils::FilePath_to_FileName(DownloadsJson::jsonInstance().filename(index));
+            std::string f_sizeTotal = Utils::bytesToSize(DownloadsJson::jsonInstance().fileSizeTotal(index));
+
+            DownloadsOBVector().Append(make<Torroid::implementation::Downloads>(to_hstring(f_name), to_hstring(f_sizeTotal)));
+			DownloadsOBVector().GetAt(index).IsDownloading(true);
+        }
+		startDownloadStatsUpdates();
     }
 
     Torroid::Downloads DownloadsViewModel::Downloads() {
@@ -48,8 +58,7 @@ namespace winrt::Torroid::implementation
             if (m_downloadsOBVector.GetAt(i).IsDownloading())
             {
                 updateDownloadStats(i);
-            }
-            
+            } 
         }
     }
 
@@ -57,23 +66,39 @@ namespace winrt::Torroid::implementation
     {
         if (index >= 0 && index < m_downloadsOBVector.Size()) 
         {
-            int f_speed = DownloadFile::DownloadInstance().getDownloadspeed(index);
-            int f_size = DownloadFile::DownloadInstance().getDownloadedSize(index);
-            int f_totalSize = std::stoi(DownloadFile::DownloadInstance().getFileSize(index));
-            double d_percentage = (static_cast<double>(f_size) / f_totalSize) * 100;
-            if (f_size != f_totalSize) {
+            std::string f_status = DownloadsJson::jsonInstance().fileStatus(index);
+            if (f_status == "Downloading")
+            {
+                int64_t f_sizeCurrent = DownloadFile::DownloadInstance().getCurrentFileSize(index);
+                int f_speed = DownloadFile::DownloadInstance().getDownloadspeed(index);
+                int64_t f_sizeTotal = DownloadFile::DownloadInstance().getTotalFileSize(index);
+                double d_percentage = ( (double)f_sizeCurrent / (double)f_sizeTotal ) * 100 ;
+
                 m_downloadsOBVector.GetAt(index).Speed(winrt::to_hstring(Utils::bytesToSize(std::to_string(f_speed))) + L"/s");
-                m_downloadsOBVector.GetAt(index).Size(winrt::to_hstring(Utils::bytesToSize(std::to_string(f_size))));
+                m_downloadsOBVector.GetAt(index).Size(winrt::to_hstring(Utils::bytesToSize(std::to_string(f_sizeCurrent))));
                 m_downloadsOBVector.GetAt(index).DPercentage(d_percentage);
-                m_downloadsOBVector.GetAt(index).Percentage(winrt::to_hstring(static_cast<int>(d_percentage)) + L"%");
+
+                std::string formatted_percentage = std::to_string(d_percentage);
+                size_t pos = formatted_percentage.find(".");
+                if (pos != std::string::npos && formatted_percentage.size() > pos + 3) {
+                    formatted_percentage = formatted_percentage.substr(0, pos + 3); // Cut off after two decimal places
+                }
+
+                m_downloadsOBVector.GetAt(index).Percentage( winrt::to_hstring(formatted_percentage) + L"%");
+				m_downloadsOBVector.GetAt(index).IsDownloading(true);
+        
+			}
+            else if (f_status != "Downloading")
+            {
+                std::string f_sizeCurrent = DownloadsJson::jsonInstance().fileSizeCurrent(index);
+                std::string f_sizeTotal = DownloadsJson::jsonInstance().fileSizeTotal(index);
+                std::string d_percentage = DownloadsJson::jsonInstance().fileDownloadPercentage(index);
+                m_downloadsOBVector.GetAt(index).Size(winrt::to_hstring(Utils::bytesToSize(f_sizeCurrent)));
+                m_downloadsOBVector.GetAt(index).DPercentage(static_cast<double>(std::stoi(d_percentage)));
+                m_downloadsOBVector.GetAt(index).Percentage(winrt::to_hstring(d_percentage) + L"%");
+                m_downloadsOBVector.GetAt(index).IsDownloading(false);
+				m_downloadsOBVector.GetAt(index).Speed(winrt::to_hstring(f_status));
             }
-            else {
-                m_downloadsOBVector.GetAt(index).Speed(L"Completed");
-                m_downloadsOBVector.GetAt(index).Size(winrt::to_hstring(Utils::bytesToSize(std::to_string(f_totalSize))));
-                m_downloadsOBVector.GetAt(index).DPercentage(static_cast<double>(100));
-                m_downloadsOBVector.GetAt(index).Percentage(winrt::to_hstring(L"100%"));
-            }
-            
         }
     }
 }
